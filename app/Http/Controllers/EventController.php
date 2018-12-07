819f84b07ed1eeeef78fa574912e464f14b640c9
Christian Powell <christiant.powell@gmail.com>
2018-12-06 18:46:16 -0600

    Add child views for create,edit,delete

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -16,10 +16,13 @@ class EventController extends Controller
         ]);
     }
 
-
-    public function search()
+    public function search(Request $request)
     {
         return view('events.search');
+    }
+
+    public function searchProcess(Request $request)
+    {
     }
 
     public function login()
@@ -27,6 +30,49 @@ class EventController extends Controller
         return view('events.login');
     }
 
+    /**
+     * GET /books/create
+     * Display the form to add a new book
+     */
+    public function create(Request $request)
+    {
+        return view('events.create');
+    }
 
+    /**
+     * POST /books
+     * Process the form for adding a new book
+     */
+    public function store(Request $request)
+    {
+    }
 
+    public function edit($id)
+    {
+        return view('events.edit');
+    }
+
+    /*
+    * PUT /books/{id}
+    */
+    public function update(Request $request)
+    {
+    }
+
+    /*
+   * Asks user to confirm they actually want to delete the book
+   * GET /books/{id}/delete
+   */
+    public function delete()
+    {
+        return view('events.delete');
+    }
+
+    /*
+    * Actually deletes the book
+    * DELETE /books/{id}/delete
+    */
+    public function destroy()
+    {
+    }
 }
